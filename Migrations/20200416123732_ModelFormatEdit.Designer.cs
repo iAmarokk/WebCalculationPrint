122aa726a1f0bb17979b00d7e6845860da5f3da6
Александр Небайкин <iamarokk@gmail.com>
2020-04-16 15:41:24 +0300

    Add DB

--- Migrations/20200416123732_ModelFormatEdit.Designer.cs
+++ Migrations/20200416123732_ModelFormatEdit.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebCalculationPrint.Data;
 
 namespace WebCalculationPrint.Migrations
 {
     [DbContext(typeof(WebCalculationPrintContext))]
-    partial class WebCalculationPrintContextModelSnapshot : ModelSnapshot
+    [Migration("20200416123732_ModelFormatEdit")]
+    partial class ModelFormatEdit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200416123732_ModelFormatEdit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebCalculationPrint.Migrations
+{
+    public partial class ModelFormatEdit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ColourfulnessRate",
+                table: "Format",
+                newName: "FormatRate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FormatRate",
+                table: "Format",
+                newName: "ColourfulnessRate");
+        }
+    }
+}
