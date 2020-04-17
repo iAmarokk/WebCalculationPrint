31e2f49f86d301d52443137453b82feebf99efb8
Александр Небайкин <iamarokk@gmail.com>
2020-04-17 19:40:25 +0300

    Migrate to SQLite

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace WebCalculationPrint
             services.AddRazorPages();
 
             services.AddDbContext<WebCalculationPrintContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebCalculationPrintContext")));
+                    options.UseSqlite(Configuration.GetConnectionString("WebCalculationPrintContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
